4e74ee0710783ba06785848a17144c23f0788242
JuanquiZabalo <jucazato95@gmail.com>
2023-12-04 22:04:20 +0100

    Anadir ejercicio 4

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+/*Crea una Promise que se rechace después de 2 segundos y captura el 
+error para imprimir "Error: Promise rechazada" */
+
+const miPromesa = new Promise(function(resolve, reject){
+    setTimeout(function(){reject('Promesa rechazada')})
+}, 2000)
+
+miPromesa.catch((error)=>{
+    console.log('Error:',error)
+})
