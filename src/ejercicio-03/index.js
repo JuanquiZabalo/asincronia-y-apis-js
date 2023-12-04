60ba79d647155ba8be981bd1acb1227db8fce0fb
JuanquiZabalo <jucazato95@gmail.com>
2023-12-04 18:59:55 +0100

    Anadir ejercicio 3

--- src/ejercicio-03/index.js
+++ src/ejercicio-03/index.js
@@ -1,1 +1,21 @@
 // Escribe tu código aquí:
+/*Define una función asincrónica que espere 1 segundo y luego devuelva 
+una cadena que diga "Operación completada". Utiliza async/await. */
+// async delante de una function hace que la funcion devuelva una promesa
+// await suspende la funcion y espera por el resultado de la promesa
+
+async function operacionAsincrona(){
+    const promesa = await new Promise((resuelta)=>{
+        setTimeout(function(){
+            resuelta('Operacion resuelta')
+        }, 3000)
+    })
+    return promesa
+}
+
+async function ejecutarOperacion(){
+    const resultado = await operacionAsincrona()
+    console.log(resultado)
+}
+
+ejecutarOperacion()
