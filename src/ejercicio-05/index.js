ca3f428c1945dad3b10022c024322069d4cada70
JuanquiZabalo <jucazato95@gmail.com>
2023-12-06 23:43:07 +0100

    Anadir ejercicio 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,35 @@
 // Escribe tu código aquí:
+/*Crea tres Promises consecutivas, donde cada una se resuelva 
+después de 1 segundo y devuelva un número diferente. Luego, 
+encadena las tres Promises para sumar los resultados y mostrar 
+el resultado final.*/
+
+function crearPromesa(valor, retraso){
+    const promesa = new Promise(function(resuelta){
+        setTimeout(()=>{
+            resuelta(valor)
+        }, retraso)
+    })
+    return promesa
+}
+
+let numero1
+let numero2
+let numero3
+
+crearPromesa(1, 1000).then((resultado)=>{
+    console.log(resultado)
+    numero1 = resultado
+    return crearPromesa(2, 1000)
+})
+.then((resultado2)=>{
+    console.log(resultado2)
+    numero2 = resultado2
+    return crearPromesa(3, 1000)
+})
+.then((resultado3)=>{
+    console.log(resultado3)
+    numero3 = resultado3
+    const suma = numero1 + numero2 + numero3
+    console.log('Suma: ',suma)
+})
