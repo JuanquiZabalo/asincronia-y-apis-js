12b496b6620f6000dde3328e1eb3ccbf94002ff5
JuanquiZabalo <jucazato95@gmail.com>
2024-01-04 15:50:11 +0100

    Anadir ejercicio 11

--- src/ejercicio-11/index.js
+++ src/ejercicio-11/index.js
@@ -1,1 +1,25 @@
-// Escribe tu código aquí:
+/**Realiza tres Promises, algunas de las cuales se resuelvan y 
+ * otras se rechacen. Utiliza Promise.allSettled() para obtener 
+ * información sobre el estado de todas las Promises. */
+
+const promise1 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve(1)
+    }, 1000)
+})
+
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject('Error en promesa 2')
+    }, 3000)
+})
+
+const promise3 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve(3)
+    }, 2000)
+})
+
+Promise.allSettled([promise1, promise2, promise3]).then((resultados)=>{
+    console.log("Estado de las promesas: ", resultados)
+})
